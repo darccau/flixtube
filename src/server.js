1aa2539f264b16bedf384506f735a6c049226440
duardopx <darccau@protonmail.com>
2021-09-19 20:48:58 -0300

    feat: serving sample video

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
-const express = require("express");
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.get("/", (req, res) => {
-  res.send({ test: "test" });
-});
+app.get('/video', (req, res) => {
+  const videoPath = path.resolve(__dirname, '..', 'videos', 'sample.mp4')
+
+  fs.stat(videoPath, (err, stats) => {
+    if (err) {
+      console.error('An error occurred ')
+      res.sendStatus(500)
+      return
+    }
+
+    res.writeHead(200, {
+      'Content-Length': stats.size,
+      'Content-Type': 'video/mp4',
+    })
+    fs.createReadStream(videoPath).pipe(res)
+  })
+})
 
 app.listen(PORT, () => {
-  console.log(`[*] Listen on http://localhost:${PORT}`);
-});
+  console.log(`[*] Listen on http://localhost:${PORT}`)
+})
