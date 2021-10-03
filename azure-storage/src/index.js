4c2bd7f1f6ded653159931f98a874476e8a5f554
duardopx <duardopx@protonmail.com>
2021-10-03 09:06:50 -0300

    feat: initial consult for azure storage

---
+++ azure-storage/src/index.js
@@ -0,0 +1,51 @@
+import express from 'express' 
+import azure from 'azure-storage'
+
+const app = express()
+
+const PORT = process.env.PORT
+const STORAGE_ACCOUNT_NAME = process.env.STORAGE_ACCOUNT_NAME
+const STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY
+
+console.log(PORT)
+console.log(STORAGE_ACCOUNT_NAME)
+console.log(STORAGE_ACCESS_KEY)
+
+const createBlobService = () => {
+  const blobService = azure.createBlobService(STORAGE_ACCOUNT_NAME,
+    STORAGE_ACCESS_KEY)
+  return blobService
+}
+
+app.get('/video', (req, res) => {
+
+  const videoPath = req.query.path
+  const blobService = createBlobService()
+
+  const containerName = 'videos'
+  blobService.getBlobProperties(containerName,
+    videoPath, (err, propertier) => {
+    if (err) {
+      res.sendStatus(500)
+      return
+    }
+
+    res.writeHead(200, {
+      'Contentype-Length': propertier.contentLength,
+      'Content-Type': 'video/mp4'
+    })
+
+      blobService.getBlobToStream(containerName,
+        videoPath, res, err => {
+          if (err) {
+            res.sendStatus(500)
+            return
+          }
+      })
+  })
+}) 
+
+app.listen(PORT, () => {
+  console.log(`Server Running on localhost:${PORT}`)
+})
+
