115efc32c88c110edb436e64b6fe0bfa82381376
duardopx <duardopx@protonmail.com>
2021-10-04 11:57:38 -0300

    feat: redirection content from video-storage

--- video-streaming/src/server.js
+++ video-streaming/src/server.js
@@ -4,31 +4,36 @@ const http = require('http')
 const app = express()
 
 if (!process.env.PORT) {
-  throw Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw Error("Especify environment variable [PORT]")
 }
 
 if (!process.env.VIDEO_STORAGE_HOST) {
-  throw Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+  throw Error("Especify environment variable [VIDEO_STORAGE_HOST]")
 }
 
 if (!process.env.VIDEO_STORAGE_PORT) {
-    throw Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+  throw Error("Especify environment variable [VIDEO_STORAGE_PORT]")
 }
 
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+
 app.get("/video", (req, res) => {
-    const forwardRequest = http.request( // Forward the request to the video storage microservice.
-        {
-            host: VIDEO_STORAGE_HOST,
-            port: VIDEO_STORAGE_PORT,
-            path: '/video?path=SampleVideo_1280x720_1mb.mp4', // Video path is hard-coded for the moment.
-            method: 'GET',
-            headers: req.headers
-        }, 
-        forwardResponse => {
-            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-            forwardResponse.pipe(res);
-        }
-    );
+  const forwardRequest = http.request( 
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: '/video?path=sample.mp4', 
+      method: 'GET',
+      headers: req.headers
+    }, 
+    forwardResponse => {
+      res.writeHeader(forwardResponse.statusCode,
+        forwardResponse.headers);
+      forwardResponse.pipe(res);
+    }
+  );
     
     req.pipe(forwardRequest);
 });
