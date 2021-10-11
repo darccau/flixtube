a4f5f438449ff9cbeb589b2d4d6ab1b502eae944
darccau <darccau@protonmail.com>
2021-10-10 23:42:16 -0300

    feat: integrate mongodb on video-streaming

--- video-streaming/src/server.js
+++ video-streaming/src/server.js
@@ -1,43 +1,59 @@
 const express = require('express')
+const mongodb = require('mongodb')
 const http = require('http')
 
 const app = express()
 
-if (!process.env.PORT) {
-  throw Error("Especify environment variable [PORT]")
-}
-
-if (!process.env.VIDEO_STORAGE_HOST) {
-  throw Error("Especify environment variable [VIDEO_STORAGE_HOST]")
-}
-
-if (!process.env.VIDEO_STORAGE_PORT) {
-  throw Error("Especify environment variable [VIDEO_STORAGE_PORT]")
-}
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
+const DBHOST = process.env.DBHOST
+const DBNAME = process.env.DBNAME
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 
-app.get("/video", (req, res) => {
-  const forwardRequest = http.request( 
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: '/video?path=sample.mp4', 
-      method: 'GET',
-      headers: req.headers
-    }, 
-    forwardResponse => {
-      res.writeHeader(forwardResponse.statusCode,
-        forwardResponse.headers);
-      forwardResponse.pipe(res);
-    }
-  );
-    
-    req.pipe(forwardRequest);
-});
+function main() {
+  return mongodb.MongoClient.connect(DBHOST)
+    .then(client => {
+      const db = client.db(DBNAME)
+      const videosCollection = db.collection('videos')
 
-app.listen(PORT, () => {
-  console.log(`[*] Listen on http://localhost:${PORT}`)
-})
+      app.get("/video", (req, res) => {
+        const videoId = new mongodb.ObjectID(req.query.id)
+        videosCollection
+          .findOne({_id: videoId})
+          .then(videoRecord => {
+            if(!videoRecord) {
+              res.sendStatus(500)
+              return;
+            }
+
+            const forwardRequest = http.request(
+              {
+                host: VIDEO_STORAGE_HOST,
+                port: VIDEO_STORAGE_PORT,
+                path: `/video?path=${videoRecord}.videoPath`,
+                method: 'GET',
+                headers: req.headers
+              }
+            )
+            req.pipe(forwardRequest)
+          })
+          .catch(err => {
+            console.error('Database query failed')
+            console.error(err && err.stack || err)
+            res.sendStatus(500)
+          })
+      })
+
+      app.listen(PORT, () => {
+        console.log(`[*] Listen on http://localhost:${PORT}`)
+      })
+    })
+}
+
+
+main()
+  .then(() => console.log('Microservice online'))
+  .catch(err => {
+    console.error('Microservice failed to start')
+    console.error(err && err.stack || err)
+  })
