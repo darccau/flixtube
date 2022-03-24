2bfb9ba4a5e7c9736f5093febb534c0099d3f321
darccau <darccau@protonmail.com>
2022-03-24 11:53:18 -0300

    feat(*): update to chapter 7

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "flixtube-terraform"
+    storage_account_name = "terr4fm"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
