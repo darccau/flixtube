87c5c058bb53d93186d07881ddd55c41e2fc3f52
darccau <darccau@protonmail.com>
2022-03-25 17:24:44 -0300

    fix(terraform): backend configurations

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "flixtube-terraform"
+    resource_group_name  = "NetworkWatcherRG"
     storage_account_name = "terr4fm"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
